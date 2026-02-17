e7b4d53d5d02192b72a444fab217d7af649eb996
Andres Torres <andres.torres03@proton.me>
2026-02-17 00:00:48 -0700

    feat: mostly replicated the results

--- notebooks/blink_util.py
+++ notebooks/blink_util.py
@@ -9,25 +9,27 @@ def twopi_normalize(phi_n1, phi_n2, coef, verbose=False):
     
     :param phi_n1: phi_sub_n1 where n1 is an integer
     :param phi_n2: phi_sub_n2 where n2 is an integer not equals n1
-    :param coef: coefficient which is equals integer n1 of phi_sub_n1
+    :param coef: coefficient which is equals integer n2 of phi_sub_n1
     :param verbose: True to print old and new values for normalization
+
+    :returns phi_sub_n2n1: Ex: for n2=3 and n1=1 returns Phi_31
     """
 
-    phi_sub_n1n2 = phi_n2 - coef*phi_n1
+    phi_sub_n2n1 = phi_n2 - coef*phi_n1  
     
 
-    for i in range(len(phi_sub_n1n2)):
-        Val = phi_sub_n1n2[i]
+    for i in range(len(phi_sub_n2n1)):
+        Val = phi_sub_n2n1[i]
         OldVal = Val
         while Val < 0:
             Val += 2*np.pi
         while Val > 2*np.pi:
             Val -= 2*np.pi
-        phi_sub_n1n2[i] = Val
+        phi_sub_n2n1[i] = Val
         if verbose == True:
             print(f'Was {OldVal} and now is {Val}.')
 
-    return phi_sub_n1n2
+    return phi_sub_n2n1
 
 
 
@@ -36,7 +38,7 @@ def get_FoH(df, verbose=False):
     phi1 = df['phi1'].to_numpy()
     phi3 = df['phi3'].to_numpy()
 
-    print(P)
+    #print(P)
 
     phi31 = twopi_normalize(phi1,phi3,coef=3,verbose=verbose)
 
