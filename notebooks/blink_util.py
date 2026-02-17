1440dbe3ab3e813a8dfd0665dc0be52f56f71bc3
Andres Torres <andres.torres03@proton.me>
2026-02-16 18:42:54 -0700

    feat: add alpha ratios and phi param relations w/ normalization

--- notebooks/blink_util.py
+++ notebooks/blink_util.py
@@ -1,26 +1,45 @@
 import numpy as np
 
 
-def get_FoH(df, verbose=False):
-    P = df['period'].to_numpy()
-    phi1 = df['phi1'].to_numpy()
-    phi3 = df['phi3'].to_numpy()
+def twopi_normalize(phi_n1, phi_n2, coef, verbose=False):
+    """
+    Docstring for twopi_normalize
 
-    phi31 = phi3 - 3*phi1
-    phi31
+    Generates phi_sub_n1n2 where phi_sub_n1n2 = phi_sub_n2 - N2*phi_sub_n1
+    
+    :param phi_n1: phi_sub_n1 where n1 is an integer
+    :param phi_n2: phi_sub_n2 where n2 is an integer not equals n1
+    :param coef: coefficient which is equals integer n1 of phi_sub_n1
+    :param verbose: True to print old and new values for normalization
+    """
 
-    for i in range(len(phi31)):
-        Val = phi31[i]
+    phi_sub_n1n2 = phi_n2 - coef*phi_n1
+    
+
+    for i in range(len(phi_sub_n1n2)):
+        Val = phi_sub_n1n2[i]
         OldVal = Val
         while Val < 0:
             Val += 2*np.pi
         while Val > 2*np.pi:
             Val -= 2*np.pi
-        phi31[i] = Val
+        phi_sub_n1n2[i] = Val
         if verbose == True:
             print(f'Was {OldVal} and now is {Val}.')
 
-    phi31
+    return phi_sub_n1n2
+
+
+
+def get_FoH(df, verbose=False):
+    P = df['period'].to_numpy()
+    phi1 = df['phi1'].to_numpy()
+    phi3 = df['phi3'].to_numpy()
+
+    print(P)
+
+    phi31 = twopi_normalize(phi1,phi3,coef=3,verbose=verbose)
+
 
     Fe_on_H = -5.038-5.394*P+1.345*phi31
     return Fe_on_H
